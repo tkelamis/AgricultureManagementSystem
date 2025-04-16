67f4ba1d8a95eb04a4f499a40f6cce2b7c9ceb5f
tkelamis <kelamis.anastasios@gmail.com>
2025-04-16 07:35:49 +0300

    added ngClass and ngtemplate with else examples

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -10,17 +10,46 @@ export class AppComponent implements OnInit {
   
   title = 'AgricultureManagementSystem';
 
-  userSignedIn?: string;
+  userNameSignedIn?: string;
+  
+
 
   constructor(){}
 
   ngOnInit(): void {
     this.signInUser();
+    this.setWelcomeMessage();
+    this.setCurrentClasses();
   }
 
   signInUser(){
-    this.userSignedIn = 'Kelamis'
+    this.userNameSignedIn = 'Kelamis'
   }
 
+
+  //ng Class example
+  welcomeMessage = '';
+  userSignedIn: boolean = true;
+  isAdmin: boolean = true;
+  isFirstVisit: boolean = true;
+  currentClasses: Record<string, boolean> = {};
+
+  setWelcomeMessage() {
+    if (this.isAdmin) {
+      this.welcomeMessage = 'You have administrative privileges.';
+    } else if (this.isFirstVisit) {
+      this.welcomeMessage = 'Thank you for joining us for the first time!';
+    } else {
+      this.welcomeMessage = 'We are glad to have you back.';
+    }
+  }
+
+  setCurrentClasses(){
+    this.currentClasses = {
+    'admin-style': this.isAdmin,
+    'first-visit-style': this.isFirstVisit,
+    'returning-user-style': !this.isFirstVisit && !this.isAdmin
+    }
+  }
   
 }

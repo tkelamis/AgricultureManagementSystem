ce7750c348febd2a3aee604ead890a14f8782efe
tkelamis <kelamis.anastasios@gmail.com>
2025-04-30 13:56:13 +0300

    practiced examples about observables

--- src/app/features/practice/practice-child/practice-child.component.ts
+++ src/app/features/practice/practice-child/practice-child.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-practice-child',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './practice-child.component.css'
 })
 export class PracticeChildComponent implements OnInit {
+  practiceObservable = new Observable<string>(observableEmitter  =>{
+    observableEmitter.next('Hello');
+    observableEmitter.next('World');
+    
+    }
+  )
+
+  
+
   ngOnInit() {
-    // Throwing error after the component is loaded
-    throw new Error('Crash during initialization!')}
+
+    const subscription = this.practiceObservable.subscribe(value =>{
+      if (typeof value === 'string'){
+        console.log('The value is right and it is ',value);
+      }
+      else{
+        console.log('The value is of the wrong type');
+      }
+    })
+  
+  }
+
+    
 }
